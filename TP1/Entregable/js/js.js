fd6107d7f93bdb3f1a6d5dd71023038575f3a9c9
ManuelGosende <mgosende5@gmail.com>
2020-09-08 15:05:49 -0300

    Aplicación de filtro Negativo y avances en estilo de página

--- TP1/Entregable/js/js.js
+++ TP1/Entregable/js/js.js
@@ -4,32 +4,64 @@ let canvas = document.querySelector("#canvas");
 let ctx = canvas.getContext("2d");
 let selectImage = document.querySelector('.openImage');
 
+// ---------------------------- LIMPIAR LIENZO ----------------------------- //
+
+let cleanCanvas = document.querySelector('.clean');
+cleanCanvas.addEventListener("click", function() {
+    for(let x = 0; x < canvas.width; x++) {
+        for(let y = 0; y < canvas.height; y++) {
+            executionTool(x, y, 255, 255, 255);
+        }
+    }
+    ctx.putImageData(imageData, 0, 0);
+})
+
+let r = 0;
+let g = 0;
+let b = 0;
+
 let imageData = ctx.createImageData(canvas.width, canvas.height);
 
 // ---------------------------- CARGAR IMAGEN ----------------------------- //
 
 selectImage.onchange = e => {
     let file = e.target.files[0];
-    let reader = new FileReader();
-    reader.readAsDataURL(file);
-
-    reader.onload = readerEvent => {
-        let content = readerEvent.target.result;
-        let image = new Image();
-        image.src = content;
-
-        image.onload = function () {
-            let imageAspectRatio = (1.0 * this.height) / this.width;
-            let imageScaledWidth = canvas.width;
-            let imageScaledHeight = canvas.width * imageAspectRatio;
-
-            ctx.drawImage(this, 0, 0, imageScaledWidth, imageScaledHeight);
-
-            let imageData = ctx.getImageData(0, 0, imageScaledWidth, imageScaledHeight);
-            
-            ctx.putImageData(imageData, 0, 0);
+    if(areImg(file)) {
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+    
+        reader.onload = readerEvent => {
+            let content = readerEvent.target.result;
+            let image = new Image();
+            image.src = content;
+    
+            image.onload = function () {
+                let imageAspectRatio = (1.0 * this.height) / this.width;
+                let imageScaledWidth = canvas.width;
+                let imageScaledHeight = canvas.width * imageAspectRatio;
+    
+                ctx.drawImage(this, 0, 0, imageScaledWidth, imageScaledHeight);
+                // se resetea el canvas con la imagen.
+                imageData = ctx.getImageData(0, 0, imageScaledWidth, imageScaledHeight);
+                
+                ctx.putImageData(imageData, 0, 0);
+            }
         }
     }
+    else {
+        alert("El archivo que quiere subir no es válido.")
+    }
+}
+
+function areImg(image){
+    let isImg = true;
+    let imgType = image['type'];
+    if(imgType == 'image/jpeg' || imgType == 'image/jpg' || imgType =='image/png') {
+        isImg = true;
+    }else{
+        isImg = false;
+    }
+    return isImg;
 }
 
 // ---------------------------- DESCARGAR IMAGEN ----------------------------- //
@@ -42,9 +74,21 @@ function download() {
     downloadImage.download = "image-canvas.png";
 }
 
+// ---------------------------- SELECCIONAR HERRAMIENTA ----------------------------- //
+
+let tool = "pintar";
+let toolController = document.querySelector(".herramienta").addEventListener("change", function() {
+    let execution = document.getElementsByName("accion");
+    for(let i = 0; i < execution.length; i ++) {
+        if(execution[i].checked) {
+            tool = execution[i].value;
+        }
+    }
+})
+
 // ---------------------------- SELECCIONAR PINCEL ----------------------------- //
 
-let size = -1;
+let size = 1;
 document.querySelector(".pinceles").addEventListener("change", function() {
     let radio = document.getElementsByName("pixel");
     for(let i = 0; i < radio.length; i ++) {
@@ -54,7 +98,7 @@ document.querySelector(".pinceles").addEventListener("change", function() {
     }
 })
 
-// ---------------------------- DIBUJAR ----------------------------- //
+// ---------------------------- UTILIZAR HERRAMIENTA ----------------------------- //
 
 let action = false;
 let lines = [];
@@ -65,9 +109,19 @@ canvas.addEventListener("mousedown", function() {
 
 canvas.addEventListener("mousemove", function(e) {
     if (action) {
+        if(tool == "pintar") {
+            r = 0;
+            g = 0;
+            b = 0;
+        }
+        else {
+            r = 255;
+            g = 255;
+            b = 255;
+        }
         let x = e.pageX - this.offsetLeft;
         let y = e.pageY - this.offsetTop;
-
+        // Unirá puntos sólo cuando haya más de un pixel pintado en el canvas.
         if(lines.length > 0) {
             let distanceY = y - lines[lines.length - 1][1];
             let distanceX = x - lines[lines.length - 1][0];
@@ -85,30 +139,20 @@ canvas.addEventListener("mousemove", function(e) {
             for(let i = 0; i < aux; i ++) {
                 auxX += distanceX / aux;
                 auxY += distanceY / aux;
-                if(size > 0) {
-                    if(size == 3) {
-                        drawWithSize(x, y, 2);
-                    }
-                    if(size == 5) {
-                        drawWithSize(x, y, 4);
-                    }
+                if(size == 1) {
+                    executionTool(Math.round(auxX), Math.round(auxY), r, g, b);
                 }
                 else {
-                    draw(Math.round(auxX), Math.round(auxY));
+                    executionToolWithSize(Math.round(auxX), Math.round(auxY), size, r, g, b); 
                 }
             }
-        }
+        }   
         lines.push([x, y]);
-        if(size > 0) {
-            if(size == 3) {
-                drawWithSize(x, y, 2);
-            }
-            if(size == 5) {
-                drawWithSize(x, y, 4);
-            }
+        if(size == 1) {
+            executionTool(x, y, r, g, b);
         }
         else {
-            draw(x, y);
+            executionToolWithSize(x, y, size, r, g, b);
         }
         ctx.putImageData(imageData, 0, 0);
     }
@@ -120,31 +164,101 @@ canvas.addEventListener("mouseup", function() {
 });
 
 canvas.onmouseleave = (function() {
-    action = false
-    lines = []
+    action = false;
+    lines = [];
 })
 
-let r = 0;
-let g = 0;
-let b = 0;
-
-function draw(x, y) {
-    setPixel(imageData, x, y, r, g, b, 255);
+function executionTool(x, y, r, g, b) {
+    if(x < canvas.width && x >= 0 && y < canvas.height && y >= 0) {
+        setPixel(imageData, x, y, r, g, b, 255);
+    }
 }
 
-function drawWithSize(x, y, distance) {
-    console.log("3 pix");
+function executionToolWithSize(x, y, size, r, g, b) {
+    let distance = size - 1;
     for(let horiz = x - distance; horiz <= x + distance; horiz ++) {
         for(let vert = y - distance; vert <= y + distance; vert ++) {
-            draw(horiz, vert);
+            executionTool(horiz, vert, r, g, b);
         }
     }
 }
 
-function setPixel(imageData, y, x, r, g, b, a) {
-    let index = (y + x * imageData.width) * 4;
+function setPixel(imageData, x, y, r, g, b, a) {
+    let index = (x + y * imageData.width) * 4;
     imageData.data[index + 0] = r;
     imageData.data[index + 1] = g;
     imageData.data[index + 2] = b;
     imageData.data[index + 3] = a;
 }
+
+// ---------------------------- FILTROS ----------------------------- //
+
+function getRed(x, y) {
+    let index = (x + y * imageData.width) * 4;
+    return imageData.data[index + 0];
+}
+
+function getGreen(x, y) {
+    let index = (x + y * imageData.width) * 4;
+    return imageData.data[index + 1];
+}
+
+function getBlue(x, y) {
+    let index = (x + y * imageData.width) * 4;
+    return imageData.data[index + 2];
+}
+
+document.getElementById("negativo").addEventListener("click", function() {
+    for(let x = 0; x < canvas.width - 1; x++) {
+        for(let y = 0; y < canvas.height; y++) {
+            let newRed = 255 - getRed(x, y);
+            let newGreen = 255 - getGreen(x, y);
+            let newBlue = 255 - getBlue(x, y);
+            setPixel(imageData, x, y, newRed, newGreen, newBlue, 255);
+            //executionTool(x, y, newRed, newGreen, newBlue);
+        }
+    }
+    ctx.putImageData(imageData, 0, 0);
+});
+
+/*document.getElementById("sepia").addEventListener("click", function() {
+    for(let x = 0; x < canvas.width - 1; x++) {
+        for(let y = 0; y < canvas.height; y++) {    
+                let average = Math.floor((getRed(x, y) + getGreen(x, y) + getBlue(x, y))/3);
+                let newRed = Math.min(average + 40, 255);
+                let newGreen = Math.min(average + 15, 255);
+                let newBlue = Math.min(average, 255);
+                setPixel(imageData, x, y, newRed, newGreen, newBlue, 255);
+            }
+        }
+        ctx.putImageData(imageData, 0, 0);
+    });
+
+    document.getElementById("brillo").addEventListener("click", function() {
+        let variation = 3;
+        for(let x = 0; x < canvas.width - 1; x++) {
+            for(let y = 0; y < canvas.height; y++) {
+                let newRed = Math.min(getRed(x, y) + variation, 255)
+                let newGreen = Math.min(getGreen(x, y) + variation, 255)
+                let newBlue = Math.min(getBlue(x, y) + variation, 255)
+                setPixel(imageData, x, y, newRed, newGreen, newBlue, 255)
+            }
+        }
+        ctx.putImageData(imageData, 0, 0);
+    });
+
+    document.getElementById("binarizacion").addEventListener("click", function(){
+        let umbral = 50;
+        for(let x = 0; x < canvas.width - 1; x++) {
+            for(let y = 0; y < canvas.height; y++) {
+                let average = Math.floor((getRed(x, y) + getGreen(x, y) + getBlue(x, y))/3);
+                if(average > umbral) {
+                    setPixel(imageData, x, y, 255, 255, 255, 255);
+                }else {
+                    setPixel(imageData,x, y, 0, 0, 0, 255);
+                }
+            }
+        }
+        ctx.putImageData(imageData,0,0);
+    });*/
+
