da56e58fa532cdf74772a649c0936c661e6966b4
ManuelGosende <mgosende5@gmail.com>
2020-10-19 00:21:55 -0300

    estructura de formulario

--- TP3/Entregable/js/coments.js
+++ TP3/Entregable/js/coments.js
@@ -1,19 +1,40 @@
 "use strict";
 
 let menu = document.getElementsByTagName("header");
+let tituloMov = document.querySelectorAll(".tituloMov");
+tituloMov[0].style.animation = `deslizarTitulo 3s 1`;
+tituloMov[0].style.visibility = "visible";
+for(let t = 1; t < tituloMov.length; t ++) {
+  tituloMov[t].style.visibility = "hidden";
+}
+let caraIzq = document.querySelector(".caraIzq");
+let caraDer = document.querySelector(".caraDer");
+let form = document.getElementsByTagName("form");
+form[0].style.opacity = "0";
 
 window.onscroll = function() {
     let scrolling = window.scrollY;
-    if(scrolling > 80) {
+    if(scrolling > 120) {
         menu[0].style.backgroundColor = "rgba(26, 26, 26, 0.960)";
         ulDesplegable.style.backgroundColor = "rgba(26, 26, 26, 0.960)";
         menu[0].style.transition = "2s";
         ulDesplegable.style.transition = "2s";
+        tituloMov[1].style.animation = `deslizarTitulo 3s 1`;
+        tituloMov[1].style.visibility = "visible";
     }
     else {
         menu[0].style.backgroundColor = "rgb(5, 5, 5)";
         ulDesplegable.style.backgroundColor = "rgb(5, 5, 5)";
         menu[0].style.transition = "2s";
         ulDesplegable.style.transition = "2s";
+    }
+    if(scrolling > 570) {
+        caraIzq.style.transition = "2s";
+        caraDer.style.transition = "2s";
+        caraIzq.style.transform = `translateX(-700px)`;
+        caraDer.style.transform = `translateX(700px)`;
+        form[0].style.animationDelay = "1s";
+        form[0].style.transition = "2s";
+        form[0].style.opacity = "1";
     }
 }
