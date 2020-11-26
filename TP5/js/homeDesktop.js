396062284fcf21d18dba14ec93949995c5898748
Manuel Gosende <mgosende5@gmail.com>
2020-11-26 11:46:22 -0300

    cargado de imagenes y resolución de user en desktop

---
+++ TP5/js/homeDesktop.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/* Ocultar listas de usuario */
+
+let buttonUserDesktop = document.querySelectorAll(".buttonCollections");
+let collectionsUser = document.querySelectorAll(".contenedorListaUser");
+let lastClickedDesktop = null;
+
+for (let boton = 0; boton < buttonUserDesktop.length; boton ++) {
+    buttonUserDesktop[boton].addEventListener("click", function() {
+        collectionsUser[boton].style.transition = "2s"; 
+        if (collectionsUser[boton] == lastClickedDesktop) {
+            collectionsUser[boton].style.display = "none";
+            buttonUserDesktop[boton].style.width = "200px";
+            buttonUserDesktop[boton].style.borderRadius = "40px";
+            lastClickedDesktop = null;
+        } else {
+            collectionsUser[boton].style.display = "flex";
+            buttonUserDesktop[boton].style.borderRadius = "40px 40px 0px  0px";
+            buttonUserDesktop[boton].style.width = "300px";
+            lastClickedDesktop = collectionsUser[boton];
+        }
+        ocultarRestantes(collectionsUser[boton]);
+    });
+}
+
+function ocultarRestantes(botonSeleccionado) {
+    for (let boton = 0; boton < buttonUserDesktop.length; boton ++) {
+        if(collectionsUser[boton] != botonSeleccionado) {
+            collectionsUser[boton].style.display = "none";
+            buttonUserDesktop[boton].style.width = "200px";
+            buttonUserDesktop[boton].style.borderRadius = "40px";
+            collectionsUser[boton].style.transition = "2s";
+        }
+    }
+}
